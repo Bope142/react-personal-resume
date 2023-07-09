9601072bb89ae53ca8fa0d9e1ba8f9b7a6a4abb5
Norbert Yemuang <66695064+Bope142@users.noreply.github.com>
2023-07-09 01:46:29 +0100

    adding home page

--- src/components/iconsBulle.jsx
+++ src/components/iconsBulle.jsx
@@ -4,8 +4,16 @@ import "../assets/style/components/icons.bulle.scss";
 
 export default function IconsBulleLink(props) {
   return (
-    <Link className="icons-round" to={props.pathLink}>
+    <Link className="icons-round icons-link" to={props.pathLink}>
       <props.icons></props.icons>
     </Link>
   );
 }
+
+export const IconsBulle = (props) => {
+  return (
+    <div className="icons-bulle">
+      <props.icons></props.icons>
+    </div>
+  );
+};
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,72 @@
 import React from "react";
+import "../assets/style/pages/home.page.scss";
+import profilUser from "../assets/images/profil.png";
+import { ButtonIcons } from "../components/button";
+import { ReactComponent as linking_icons } from "../assets/icons/linking.svg";
+import { IconsBulle } from "../components/iconsBulle";
+import { ReactComponent as phone_icons } from "../assets/icons/phone.svg";
+import { ReactComponent as envelope_icons } from "../assets/icons/envelope.svg";
+import { ReactComponent as github_icons } from "../assets/icons/github.svg";
+import { ReactComponent as facebook_icons } from "../assets/icons/facebook_f.svg";
+import { ReactComponent as instagram_icons } from "../assets/icons/instagram.svg";
+import IconsBulleLink from "../components/iconsBulle";
 
+const TextOverivew = () => {
+  return (
+    <div className="text__overview">
+      <span>MY NAME</span>
+      <span>
+        IS <strong>NORBERT YEMUANG...</strong>
+      </span>
+    </div>
+  );
+};
+const MyOccupation = () => {
+  return (
+    <div className="my__occupation">
+      <strong>Software Engineer</strong> based in <strong>DRC</strong>
+    </div>
+  );
+};
+const Contact = () => {
+  return (
+    <div className="social__media_contact">
+      <div className="infos__social_media">
+        <IconsBulle icons={phone_icons} /> <p>+248458895578</p>
+      </div>
+      <div className="infos__social_media">
+        <IconsBulle icons={envelope_icons} /> <p>irnorbertyemuang@gamil.com</p>
+      </div>
+    </div>
+  );
+};
+
+const LinkSocialMedia = () => {
+  return (
+    <div className="links">
+      <IconsBulleLink pathLink="/" icons={github_icons}></IconsBulleLink>
+      <IconsBulleLink pathLink="/" icons={facebook_icons}></IconsBulleLink>
+      <IconsBulleLink pathLink="/" icons={instagram_icons}></IconsBulleLink>
+    </div>
+  );
+};
 export default function HomePage() {
-  return <div>welcome to home page</div>;
+  return (
+    <main className="container__home_page page__setcion">
+      <div className="overview__profil">
+        <TextOverivew />
+        <MyOccupation />
+        <ButtonIcons
+          classColorBtn="btn-clr-black"
+          label="Let's talk with me "
+          icons={linking_icons}
+        />
+        <Contact />
+      </div>
+      <div className="cover__profil">
+        <img src={profilUser} alt="pic user" />
+        <LinkSocialMedia />
+      </div>
+    </main>
+  );
 }
